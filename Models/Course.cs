d720fcf49a6a78de00bbd8bc1da847153d1983eb
ToanPham234 <phamthanhtoan00hutech@gmail.com>
2021-07-10 23:55:57 +0700

    Initial commit

---
+++ Models/Course.cs
@@ -0,0 +1,39 @@
+﻿namespace BigSchool.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Course")]
+    public partial class Course
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Course()
+        {
+            Attendances = new HashSet<Attendance>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string LecturerID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int CategoryID { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Attendance> Attendances { get; set; }
+
+        public List<Category> listcategory = new List<Category>();
+        public virtual Category Category { get; set; }
+        public string Name;
+    }
+}
